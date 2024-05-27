c4f206354d20845bee27b557ce4e95a582edcb5a
Weberton Alves <walvesdevs@gmail.com>
2024-05-27 13:50:55 -0300

    Tipos de dados primitivos

---
+++ aula10/index.js
@@ -0,0 +1,12 @@
+// TIPOS DE DADOS PRIMITIVOS 
+
+
+const nome= 'Weberton'; //String 
+const nome1= "Weberton"; //String 
+const nome2= `Weberton`; //String 
+const num1= 10; //number
+const num2= 10.52;//number
+let nomeAluno; // undefined = não aponta pra lugar nenhum na memoria
+const sobrenomeAluno= null; //Nulo = não aponta pra lugar nenhum na memoria
+const boolean=  false // boolean =true ou false (logico)
+

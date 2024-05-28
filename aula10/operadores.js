5da26d64c01c72ef3fe26bb963d1f548f334ad5c
Weberton Alves <walvesdevs@gmail.com>
2024-05-28 10:33:43 -0300

    operadores aritmecos

---
+++ aula10/operadores.js
@@ -0,0 +1,21 @@
+/**
+ *  Operadores Aritmeticos + - / * 
+ *  +Adição / Concatenação
+ *  (**) Potenciação
+ *  % Resto da divisão 
+ *  Ordem de prioridade de valores () ** * / % + -
+ *  Operador de Incremento ++ 
+ *  Operador de Decremento --
+ *  NaN - Not a Number
+ *  Conversão de String --
+ *  parseInt - converte uma String para um Number inteiro
+ *  parseFloat - retorna o numero flutuante (numeros quebrados/ ou com casas Decimais).
+ *  Number - vai pegar tanto como numero inteiro como numero flutuante 
+ * */
+let contador = 10;
+contador--; //2 
+contador--; //3 
+contador--; //4
+contador--; //5
+console.log(contador)
+

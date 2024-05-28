ddc10cdc7c45083b562946777391e1d232a1ae09
Weberton Alves <walvesdevs@gmail.com>
2024-05-28 11:25:46 -0300

    alert confirm e prompt

---
+++ aula11/js/script.js
@@ -0,0 +1,13 @@
+
+/* 
+*window.alert - notificação de msg 
+*window.confirm - notificação se quer ou nao executar algo 
+*window.prompt - informação sobre cliente /nome 
+*/
+let num1= prompt ('Digite um numero')
+let num2= prompt ('Digite um numero')
+num1 = Number (num1);
+num2= Number (num2);
+const resultado = num1 + num2;
+alert (`O resultado da sua conta foi:' ${resultado}`); //Template String 
+
